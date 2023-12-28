24c55fc989b27a649fcf7d317430f8f01f3a07ac
L-JM3214 <lactorjm4@gmail.com>
2023-12-28 12:17:03 +0300

    Updated

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,42 +1,82 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your-database-file.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+CORS(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Import models after initializing db
-from app.models import Hero, Power, HeroPower
-
-# Routes
-
-@app.route('/heroes')
+@app.route('/heroes', methods=['GET'])
 def get_heroes():
+    from models import Hero 
     heroes = Hero.query.all()
-    return jsonify([hero.serialize() for hero in heroes])
+    return jsonify([{'id': hero.id, 'name': hero.name, 'super_name': hero.super_name} for hero in heroes])
 
-@app.route('/heroes/<int:hero_id>')
+@app.route('/heroes/<int:hero_id>', methods=['GET'])
 def get_hero(hero_id):
     hero = Hero.query.get(hero_id)
     if not hero:
         return jsonify({"error": "Hero not found"}), 404
-    return jsonify(hero.serialize())
 
-@app.route('/powers')
+    powers = [{'id': power.id, 'name': power.name, 'description': power.description} for power in hero.powers]
+    return jsonify({'id': hero.id, 'name': hero.name, 'super_name': hero.super_name, 'powers': powers})
+
+@app.route('/powers', methods=['GET'])
 def get_powers():
     powers = Power.query.all()
-    return jsonify([power.serialize() for power in powers])
+    return jsonify([{'id': power.id, 'name': power.name, 'description': power.description} for power in powers])
 
-@app.route('/powers/<int:power_id>')
-def get_power(power_id):
+@app.route('/powers/<int:power_id>', methods=['GET', 'PATCH'])
+def get_or_patch_power(power_id):
     power = Power.query.get(power_id)
     if not power:
         return jsonify({"error": "Power not found"}), 404
-    return jsonify(power.serialize())
+
+    if request.method == 'GET':
+        return jsonify({'id': power.id, 'name': power.name, 'description': power.description})
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        if 'description' not in data:
+            return jsonify({"error": "Description is required for PATCH"}), 400
+
+        power.description = data['description']
+
+        try:
+            db.session.commit()
+            return jsonify({'id': power.id, 'name': power.name, 'description': power.description})
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": str(e)}), 400
+
+@app.route('/hero_powers', methods=['POST'])
+def create_hero_power():
+    data = request.get_json()
+    hero_id = data.get('hero_id')
+    power_id = data.get('power_id')
+    strength = data.get('strength')
+
+    if not all([hero_id, power_id, strength]):
+        return jsonify({"error": "All fields are required"}), 400
+
+    hero = Hero.query.get(hero_id)
+    power = Power.query.get(power_id)
+    if not hero or not power:
+        return jsonify({"error": "Hero or Power not found"}), 404
+
+    hero_power = HeroPower(hero=hero, power=power, strength=strength)
+    db.session.add(hero_power)
+
+    try:
+        db.session.commit()
+        return jsonify(hero_power.serialize()), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(port=5555)
--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import DateTime
 
 db = SQLAlchemy()
 
@@ -8,7 +10,30 @@ class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     super_name = db.Column(db.String(255), nullable=False)
-    powers = db.relationship('Power', secondary='hero_powers')
+    created_at = db.Column(DateTime, default=datetime.utcnow)
+    updated_at = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Relationship
+    powers = db.relationship('Power', secondary='hero_powers', backref='heroes')
+
+    def __repr__(self):
+        return f"Hero(id={self.id}, name='{self.name}', super_name='{self.super_name}', created_at='{self.created_at}', updated_at='{self.updated_at}')"
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String(255), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
+    created_at = db.Column(DateTime, default=datetime.utcnow)
+    updated_at = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    hero = db.relationship('Hero', backref=db.backref('hero_powers', cascade='all, delete-orphan'))
+    power = db.relationship('Power', backref=db.backref('power_heroes', cascade='all, delete-orphan'))
+
+    def __repr__(self):
+        return f"HeroPower(id={self.id}, strength='{self.strength}', hero_id={self.hero_id}, power_id={self.power_id}, created_at='{self.created_at}', updated_at='{self.updated_at}')"
 
 class Power(db.Model):
     __tablename__ = 'powers'
@@ -16,12 +41,11 @@ class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    heroes = db.relationship('Hero', secondary='hero_powers')
+    created_at = db.Column(DateTime, default=datetime.utcnow)
+    updated_at = db.Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-class HeroPower(db.Model):
-    __tablename__ = 'hero_powers'
+    # Relationship
+    heroes = db.relationship('Hero', secondary='hero_powers', backref='powers')
 
-    id = db.Column(db.Integer, primary_key=True)
-    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
-    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
-    strength = db.Column(db.String(255), nullable=False)
+    def __repr__(self):
+        return f"Power(id={self.id}, name='{self.name}', description='{self.description}', created_at='{self.created_at}', updated_at='{self.updated_at}')"
